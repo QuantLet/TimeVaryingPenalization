93dee40f888f9002abc559582d2398c32dbb1437
LZ <>
2018-10-09 17:21:09 +0200

    codes update 3

--- TVRPfmri/TVRPfmri.R
+++ TVRPfmri/TVRPfmri.R
@@ -3,7 +3,7 @@ rm(list = ls(all = TRUE))
 graphics.off()
 
 # Set working directory
-setwd("/Users/Lenka/Documents/IRTG 1792/DNAS/Simulations")
+setwd("")
 
 # Install and load packages
 libraries = c("MASS", "scales", "foreach", "doParallel", "lars", "rRAP")
@@ -20,30 +20,19 @@ registerDoParallel(cl)
 getDoParWorkers()
 
 # Load fMRI data
-setwd("/Users/Lenka/Documents/IRTG 1792/DNAS/neuroimage_data")
-load("EmotionTaskSubjects.RData")
+load("EMO.RData")
+n.objects   = 8
 data.fear   = read.table("EMO_fear_LR.txt", header = FALSE)
 data.neut   = read.table("EMO_neut_LR.txt", header = FALSE)
 
-# Adjust data sample
-final.cov = c(5, 6, 7, 14, 15, 18, 33, 35, 41, 50, 51, 52, 67, 69, 75)
-fin.obj   = c(3, 7, 9, 12, 18, 24, 26, 42)
-n.objects = length(fin.obj)
-data.new  = list()
-for (i in 1:n.objects){
-  data.new[[i]] = DatFull[[(fin.obj[i])]][, final.cov]
-}
-
-# n.objects = 47
-# data.new  = list()
-# for (i in 1:n.objects){
-#   data.new[[i]] = DatFull[[i]][, final.cov]
-# }
-
 # Computation setup for LARS
-n.nodes   = length(final.cov)      # Number of brain regions
-w         = 30                    # Length of moving windows
-m.type    = "BIC"                  # Method of choosing lambda (BIC or GCV)
+n.nodes     = 15                # Number of brain regions
+w           = 30                # Length of moving windows
+w.rap       = 20                # Burn-in period for RAP algorithm
+m.type      = "BIC"             # Method of choosing lambda (BIC or GCV)
+r.factor    = 0.95              # Forgetting factor for RAP
+s.size      = 0.025             # Step-size parameter for RAP
+lambda.init = 0.3               # Inital lambda value for RAP
 
 # Define number of nodes to regress in every core
 n.appcores = rep((n.nodes %/% n.cores), n.cores)
@@ -57,9 +46,9 @@ n.parallel = sum(n.appcores != 0)
 out_lars = list()
 for (idat in 1:n.objects){
   Sys.time()
-  data      = data.new[[idat]]
-  out_tmp   = foreach(icor = 1:n.parallel, 
-                      .packages = c("lars")) %dopar% par.lassoapp(icor)  # Parallel computing
+  data      = EMO.data[[idat]]
+  out_tmp   = foreach(icor = 1:n.parallel,            # Parallel computing
+                      .packages = c("lars")) %dopar% par.lassoapp(icor) 
   Sys.time()
   out_lars[[idat]] = res.app(n.parallel, out_tmp)
   print(idat)
@@ -71,19 +60,13 @@ for (k3 in 1:n.objects){
 }
 mean.lars = apply(lambda.lars, 1, mean)
 
-# Computation setup for RAP
-w.rap       = 20
-r.factor    = 0.95
-s.size      = 0.025 
-lambda.init = 0.3 
-
 # RAP: Lasso estimation
 out_rap = list()
 for (idat in 1:n.objects){
   Sys.time()
-  data      = data.new[[idat]]
-  out_tmp   = foreach(icor = 1:n.parallel, 
-                      .packages = c("lars", "rRAP")) %dopar% par.rapapp(icor)   # Parallel computing
+  data      = EMO.data[[idat]]
+  out_tmp   = foreach(icor = 1:n.parallel,            # Parallel computing
+                      .packages = c("lars", "rRAP")) %dopar% par.rapapp(icor)   
   Sys.time()
   out_rap[[idat]] = res.app(n.parallel, out_tmp)
   print(idat)
@@ -94,14 +77,6 @@ for (k3 in 1:n.objects){
 }
 mean.rap = apply(lambda.rap, 1, mean)
 
-# cor.data = numeric(0)
-# for (k4 in 1:n.objects){
-#   cor.data[k4] = cor(lambda.rap[-seq(1:(w - w.rap)), k4], data.neut[-seq(1:w), 1])
-# }
-# 
-# fin.obj  = which(cor.data > 0.2)
-# fin.obj  = c(9, 27, 31, 33, 42, 47)
-
 # Close cluster
 stopCluster(cl)
 
@@ -110,13 +85,13 @@ norm.lars = norm.0to1(mean.lars)
 norm.rap  = norm.0to1(mean.rap[-seq(1:(w - w.rap))])
 
 # Plot settings
-par(mfrow = c(2, 1))
+par(mfrow = c(1, 1))
 par(mar = c(5, 5, 1, 1))
 
 # Plot time series of lambda both for LARS and RAP
 plot(norm.lars, type = "l",  col = "black",
-     xlab = "Time", frame = TRUE, cex.main = 1.5, ylab = expression(paste("Average ", lambda)),
-     cex.lab = 1.2, lwd = 1)
+     xlab = "Time", frame = TRUE, cex.main = 1.5, 
+     ylab = expression(paste("Average ", lambda)), cex.lab = 1.2, lwd = 1)
 lines(norm.rap, col = "blue", lty = 2, lwd = 1.5)
 polygon(c((48 - w), max(0, (23 - w)), max(0, (23 - w)), (48 - w)), 
         c(0, 0, 1, 1), col = alpha("red2", 0.2),
@@ -139,5 +114,3 @@ polygon(c((175 - w), (169 - w), (169 - w), (175 - w)),
 legend("bottomright", c("BIC", "RAP"), col = c("black", "blue"),
        ncol = 2, cex = 0.9, lwd = 1.5, lty = c(1, 4, 2))
 
-
-data.neut
